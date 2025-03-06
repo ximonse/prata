fdd3aedb4d937a69f84edfd9f38df628327cf3b5
Simon Zetterberg <ximon.se@gmail.com>
2025-03-06 21:55:47 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,8 @@
+{
+  "name": "text-to-speech-app",
+  "version": "1.0.0",
+  "description": "En app som konverterar text till tal med OpenAI TTS API",
+  "dependencies": {
+    "axios": "^1.6.0"
+  }
+}
